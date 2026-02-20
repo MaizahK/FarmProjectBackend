eca6fe29dd6e1a4a710300a53f60c2ddd9f1a4cb
Maizah Kaleem <maizahkaleem1@gmail.com>
2026-02-21 04:10:33 +0500

    register animal models to admin

--- apps/animals/admin.py
+++ apps/animals/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models.animals import *
+from .models.vaccination_records import *
 
 # Register your models here.
+admin.site.register(Animal)
+admin.site.register(VaccinationRecord)
