07e2623e7a64cf28a7107ff40ffead6b5656cfdb
Maizah Kaleem <maizahkaleem1@gmail.com>
2026-02-21 04:17:55 +0500

    TODO: implement an rbac middleware

--- apps/animals/views.py
+++ apps/animals/views.py
@@ -4,7 +4,6 @@ from .models.animals import Animal
 from .serializers import AnimalSerializer    
 from .models.vaccination_records import VaccinationRecord
 from .serializers import VaccinationRecordSerializer
-from .utils.permissions import IsManagerUser # Using the RBAC class from earlier
 
 class AnimalViewSet(viewsets.ModelViewSet):
     queryset = Animal.objects.all()
@@ -18,16 +17,7 @@ class AnimalViewSet(viewsets.ModelViewSet):
 class VaccinationRecordViewSet(viewsets.ModelViewSet):
     queryset = VaccinationRecord.objects.all()
     serializer_class = VaccinationRecordSerializer
-
-    def get_permissions(self):
-        """
-        RBAC Logic: 
-        - Anyone authenticated can view or create records.
-        - Only Managers/Admins can delete records.
-        """
-        if self.action == 'destroy':
-            return [IsManagerUser()]
-        return [super().get_permissions()[0]]
+    permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         # Optional: Add logic here to deduct the 'vaccine_resource' 
