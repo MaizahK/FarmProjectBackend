fd15067590392be09f028a37020afe4e39b1a683
Maizah Kaleem <maizahkaleem1@gmail.com>
2026-02-22 04:33:36 +0500

    comment removed

--- apps/identity/urls.py
+++ apps/identity/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView # Import this
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import LogoutView, TokenObtainPairView, TokenVerifyView
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Add this
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='token_logout'),
     path('verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
