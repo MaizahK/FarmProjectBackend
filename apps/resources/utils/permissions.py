5ced28f843324745086b93a4e7b77ae3262bf2aa
Maizah Kaleem <maizahkaleem1@gmail.com>
2026-02-22 04:34:10 +0500

    unneeded files removed

--- apps/resources/utils/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import permissions
-
-class HasRole(permissions.BasePermission):
-    def __init__(self, allowed_roles):
-        self.allowed_roles = allowed_roles
-
-    def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role.name in self.allowed_roles
--- apps/resources/views/resources.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from FarmProjectBackend.apps.resources.models import Resource
-# from serializers import ResourceSerializer, ResourceTypeSerializer
-from ..utils.permissions import HasRole
-
-class ResourceViewSet(viewsets.ModelViewSet):
-    queryset = Resource.objects.all()
-    # serializer_class = ResourceSerializer
-    permission_classes = [HasRole(['Admin', 'Farmer'])]
