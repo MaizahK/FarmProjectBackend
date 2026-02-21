542bf5c1dca2b3265e9559c91f05e7d7f2b5332a
Maizah Kaleem <maizahkaleem1@gmail.com>
2026-02-21 05:00:13 +0500

    refresh token api added

--- apps/identity/urls.py
+++ apps/identity/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView # Import this
 from .views import LogoutView, TokenObtainPairView, TokenVerifyView
 
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'), # Add this
     path('logout/', LogoutView.as_view(), name='token_logout'),
     path('verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- farm_project_backend/settings.py
+++ farm_project_backend/settings.py
@@ -70,6 +70,7 @@ SIMPLE_JWT = {
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
+    'UPDATE_LAST_LOGIN': True,
 }
 
 MIDDLEWARE = [
