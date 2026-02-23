2465b9bc9fb6ed1325a70c2c3f618f47b5a59f0c
Maizah Kaleem <maizahkaleem1@gmail.com>
2026-02-23 05:18:34 +0500

    animal linked to product

--- apps/products/models.py
+++ apps/products/models.py
@@ -11,7 +11,14 @@ class Product(models.Model):
     product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, related_name='items')
     description = models.CharField(max_length=200, blank=True)
     quantity = models.CharField(max_length=200)
-    
+    animal = models.ForeignKey(
+        'animals.Animal', 
+        on_delete=models.SET_NULL, 
+        related_name='products',
+        null=True, 
+        blank=True
+    )
+
     def __str__(self):
         return f"{self.description} ({self.product_type.name})"
 
